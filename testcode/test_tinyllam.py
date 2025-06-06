7c81f767abd93530114fd6dafb80de54aa49570c
Seyyed Mohammad Hossein Hashemi <89422926+Amahseyn@users.noreply.github.com>
2025-06-07 01:01:40 +0330

    !

--- testcode/test_tinyllam.py
+++ testcode/test_tinyllam.py
@@ -3,7 +3,7 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import pandas as pd
 print(os.getenv("GOOGLE_API_KEY"))
-genai.configure(api_key="AIzaSyAK6PS-eVxAQDz7ELB0SEJS2VbF6DzrIHk") # Make sure your API key is set
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY")) # Make sure your API key is set
 
 
 # Load your CSV
